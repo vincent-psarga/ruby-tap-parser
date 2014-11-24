b680fe71beaa8f91de530c8be1a4deaa602ca274
Vincent Pretre <vincent.pretre@gmail.com>
2014-11-24 18:58:54 +0100

    Use tap export in travis script Remove zest-publisher from travis script, absolutelly no need for it here

--- bin/travis_script.sh
+++ bin/travis_script.sh
@@ -1,4 +1,2 @@
-gem install zest-publisher
-zest-publisher --help
-
-CODECLIMATE_REPO_TOKEN=6d77ec3b36006579893a201a729c347b1ef52d0728ec019b20b8bfffbf239a0e bundle exec rspec spec
+#!/bin/bash
+CODECLIMATE_REPO_TOKEN=6d77ec3b36006579893a201a729c347b1ef52d0728ec019b20b8bfffbf239a0e bundle exec rspec -r rspec-extra-formatters -f TapFormatter
